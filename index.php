fe31bdec84a1172e4c8930ce64c54b8c03b15795
rifqi05 <rifkipradana134@gmail.com>
2022-01-14 20:44:55 +0700

    upload pertama

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('halaman')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Lakukan apa yang harus kamu lakukan sampai kamu dapat melakukan apa yang ingin kamu lakukan.</p><br>USM_JAYA</br>
+</div>
+
+
+
+
